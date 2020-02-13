0567cc07fb38f438e4b39b6892243ee9931e7c5e
Ihor Suzdaliev <swindows101@gmail.com>
2020-02-13 19:59:00 +0100

    v1.0

---
+++ config.py
@@ -0,0 +1,6 @@
+client_id='kjwXsAvbGBE3gQ'
+client_secret='GC1Ki_7qt2_4MJLBD3YTflAx_EU'
+user_agent='linux:RPMB_bot:0.1 (by /u/suzdaliev)'
+tg_token='1071503486:AAH0r77QP-za8EE64wk7Qr1c9oYF3fHb454'
+tg_channelID=-1001358874325
+
