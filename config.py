e03aaf77ee777d47581d699a5ed3216344cdc5c6
Ihor Suzdaliev <swindows101@gmail.com>
2020-02-14 00:08:48 +0100

    aaa

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-client_id='kjwXsAvbGBE3gQ'
-client_secret='GC1Ki_7qt2_4MJLBD3YTflAx_EU'
-user_agent='linux:RPMB_bot:0.1 (by /u/suzdaliev)'
-tg_token='1071503486:AAH0r77QP-za8EE64wk7Qr1c9oYF3fHb454'
-tg_channelID=-1001358874325
-
+client_id='reddit client id'
+client_secret='reddit client secret'
+user_agent='linux:RPMB_bot:0.1 (by /u/username)'
+tg_token='tg bot token'
+tg_channelID=-target channel id
